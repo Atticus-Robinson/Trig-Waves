6079a56c5f169f0756f8d4fe4d90ca407cf50feb
Atticus-Robinson <acerobinson2@gmail.com>
2022-02-03 13:12:04 -0600

    declared starting grid parameters

---
+++ script.js
@@ -0,0 +1,19 @@
+var xScale, xMin, xMax, yMin, yMax, yScale;
+
+xScale = (Math.PI / 2);
+xMin = -2 * (Math.PI);
+xMax = 2 * (Math.PI);
+console.log('X-Scale: ' + xScale + ' X-Min: ' + xMin + ' X-Max: ' + xMax);
+
+yScale = 1;
+yMin = -10;
+yMax = 10;
+console.log('Y-Scale: ' + yScale + ' Y-Min: ' + yMin + ' Y-Max: ' + yMax);
+
+var mathBox = $('.math-box');
+var 
+
+var horizontalLine = $('<div>')
+    .addClass('h-grid-line');
+mathBox.append(horizontalLine);
+
